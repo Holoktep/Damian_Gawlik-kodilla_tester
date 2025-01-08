aea73d2fc7912440d99c8c75938352e510062bb5
Damian Gawlik <holoktep@gmail.com>
2025-01-08 16:10:08 +0100

    color

--- kodilla-intro/src/main/java/AdvCalculator.java
+++ kodilla-intro/src/main/java/AdvCalculator.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class AdvCalculator {
+    public double calculate() {
+        String userSelected = UserDialogs.getUserSelection();
+        int a = UserDialogs.getValue();
+        int b = UserDialogs.getValue();
+        double result = 0;
+        switch (userSelected) {          // [1]
+            case "ADD": result = a + b;
+            case "SUB": result = a - b;
+            case "DIV": result = a / b;
+            case "MUL": result = a * b;
+        }
+        return result;
+    }
 }
--- kodilla-intro/src/main/java/ChoseColor.java
+++ kodilla-intro/src/main/java/ChoseColor.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class ChoseColor {
+    public static String ChoseColor() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter your color: ");
+            String name = scanner.nextLine().trim();
+            if (name.length() >= 2) {
+                return name;
+            }
+            System.out.println("Color is too short. Try again.");
+        }
+    }
+
+    public static String getColorSelection() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select color (R-red, S-silver, G-green, B-black):");
+            String color = scanner.nextLine().trim().toUpperCase();
+            switch (color) {
+                case "R": return "RED";
+                case "S": return "SILVER";
+                case "G": return "GREEN";
+                case "B": return "BLACK";
+                default:
+                    System.out.println("Wrong color. Try again.");
+            }
+        }
+    }
 }
