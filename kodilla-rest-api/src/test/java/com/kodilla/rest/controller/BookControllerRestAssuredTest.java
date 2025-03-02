e64c3295572c073f1d3e01cd6e6ae117373ba7ca
Damian Gawlik <holoktep@gmail.com>
2025-03-02 17:17:25 +0100

    ZadanieApi

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerRestAssuredTest.java
@@ -0,0 +1,88 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
+import static io.restassured.RestAssured.*;
+
+@ExtendWith(MockitoExtension.class)
+class BookControllerRestAssuredTest {
+    @Mock
+    private BookService bookService;
+    @InjectMocks  // [1]
+    private BookController bookController;
+
+    @BeforeEach
+    public void initialiseRestAssuredMockMvcStandalone() {
+        RestAssuredMockMvc.standaloneSetup(bookController);  // [2]
+    }
+
+    @Test
+    void shouldFetchBooks() {
+        // given
+        Mockito.when(bookService.getBooks())
+                .thenReturn(List.of(
+                        new BookDto("Title 1", "Author 2"),
+                        new BookDto("Title 2", "Author 2"))
+                );
+        // when then
+        when()
+                .get("/books")  // [3]
+                .then()
+                .body("$.size()", Matchers.equalTo(2))  // [4]
+                .body("[0].title", Matchers.equalTo("Title 1"))    // [5]
+                .body("[0].author", Matchers.equalTo("Author 2"))
+                .body("[1].title", Matchers.equalTo("Title 2"))
+                .body("[1].author", Matchers.equalTo("Author 2"))
+                .status(HttpStatus.OK);
+    }
+
+    @Test
+    void addTest() {
+        // given
+        BookDto book = new BookDto("New Book", "New Author");
+
+        // when
+        given()
+                .contentType(ContentType.JSON)
+                .body(book)
+                .when()
+                .post("/books")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void shouldUpdatePost() {
+        Map<String, Object> updatedPostData = new HashMap<>();
+        updatedPostData.put("id", 1);
+        updatedPostData.put("title", "Updated title");
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(updatedPostData)
+                .when()
+                .put("https://jsonplaceholder.typicode.com/posts/1")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("id", Matchers.equalTo(1))
+                .body("title", Matchers.equalTo("Updated title"));
+    }
+}
