4e43a453fab49790a3b14978ff3b025a3d793329
Damian Gawlik <holoktep@gmail.com>
2025-04-13 20:07:16 +0200

    ZadanieSQL

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -1,7 +1,7 @@
 package com.kodilla.jacoco;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YearTest {
 
